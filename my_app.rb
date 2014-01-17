aed969bd9642b955f7a28cfaffb66e6463e54dfe
darrenwong916 <darrenwong06@gmail.com>
2014-01-16 18:50:05 -0800

    Links Working

---
+++ my_app.rb
@@ -0,0 +1,25 @@
+require 'sinatra'
+require 'sinatra/reloader'
+require 'typhoeus'
+require 'json'
+
+get "/" do
+	erb :index
+end
+
+post '/result' do
+  search_str = params[:movie]
+  response = Typhoeus.get("www.omdbapi.com/", :params => {:s => search_str})
+  @result = JSON.parse(response.body)
+  erb :result
+end
+
+
+
+get '/poster/:imdb' do |imdb_id|
+  # id = params[:imdb]
+  key = Typhoeus.get("www.omdbapi.com", :params => {:i => "#{imdb_id}"})
+  @poster_id = JSON.parse(key.body)
+  erb :poster
+
+end
